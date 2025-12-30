95d57b515344f97f950bf038b0fb77dc692a1b68
User <rogerioparacambi@gmail.com>
2025-12-30 15:22:03 -0300

    filtro de bisca

--- pacientes/views.py
+++ pacientes/views.py
@@ -7,4 +7,11 @@ class PacienteListView(ListView):
     template_name = 'paciente_list.html'
     context_object_name = 'pacientes'
 
-    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        nome_completo = self.request.GET.get('nome_completo')
+
+        if nome_completo:
+            queryset = queryset.filter(nome_completo__icontains=nome_completo)
+
+        return queryset
