1c8bce5327f9e1db72ce9aa8ebcecb942da76a82
User <rogerioparacambi@gmail.com>
2026-01-06 08:19:33 -0300

    atualizado

--- recepcao/views.py
+++ recepcao/views.py
@@ -5,18 +5,18 @@ from django.shortcuts import render, redirect
 from .forms import AtendimentoForm
 from .models import Atendimento
 
-def atendimento_create(request):
-    if request.method == 'POST':
-        form = AtendimentoForm(request.POST)
-        if form.is_valid():
-            atendimento = form.save(commit=False)
-            atendimento.status = 'aguardando_triagem'
-            atendimento.save()
-            return redirect('fila_triagem')
-    else:
-        form = AtendimentoForm()
+#def atendimento_create(request):
+ #   if request.method == 'POST':
+ #       form = AtendimentoForm(request.POST)
+ #       if form.is_valid():
+ #           atendimento = form.save(commit=False)
+ #           atendimento.status = 'aguardando_triagem'
+ #           atendimento.save()
+ #           return redirect('fila_triagem')
+ #   else:
+ #       form = AtendimentoForm()
 
-    return render(request, 'recepcao/atendimento_form.html', {'form': form})
+    #return render(request, 'recepcao/atendimento_form.html', {'form': form})
 
 def fila_triagem(request):
     atendimentos = Atendimento.objects.filter(
